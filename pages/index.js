2e63b1c9faec0627f76ec83d120c297fa9825f34
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-06-15 18:24:37 -0400

    update logo

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/netlify.svg" alt="Netlify Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
